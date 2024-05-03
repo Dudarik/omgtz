13487ba832f635a862308be5a82b08941cfcb87e
Александр Резниченко <aleksandr.reznichenko@korvet-jsc.ru>
2024-05-03 10:23:29 +0500

    feat: add CellsPack

---
+++ src/components/CellsPack.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="cells-pack">
+    <UnoCell
+      v-for="cell in props.cellsValues"
+      :key="cell.value"
+      :cellValue="cell"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ICellValue } from '../interfaces';
+import UnoCell from './UnoCell.vue';
+
+interface IProps {
+  cellsValues: ICellValue[];
+}
+const props = defineProps<IProps>();
+</script>
+
+<style scoped lang="scss">
+@import '../styles/const';
+
+.cells-pack {
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: $wrapperSize;
+
+  gap: 1rem;
+  justify-content: flex-start;
+}
+</style>
