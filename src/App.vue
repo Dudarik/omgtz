40e1ed65c1b649cca64eb8d013ee67fab99889e5
Александр Резниченко <aleksandr.reznichenko@korvet-jsc.ru>
2024-05-03 10:23:52 +0500

    feat: add styles and main page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import CellsPack from './components/CellsPack.vue';
+import { useCellsStore } from './store/useCellsStore';
+import { getRandomNumber } from './helpers';
 
-<template></template>
+const cellsStore = useCellsStore();
 
-<style scoped></style>
+onMounted(() => {
+  setInterval(() => {
+    const updateId = getRandomNumber(
+      0,
+      cellsStore.getVisibleCellsIds.length - 1
+    );
+    const newVal = getRandomNumber(0, 100);
+    cellsStore.setCellValue(
+      cellsStore.getVisibleCellsIds[updateId],
+      newVal.toString()
+    );
+  }, 1000);
+});
+</script>
+
+<template>
+  <div class="cellpack-wrapper" ref="observer">
+    <CellsPack
+      v-for="(item, index) in cellsStore.cells"
+      :key="index"
+      :cellsValues="item"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.cellpack-wrapper {
+  display: flex;
+  flex-direction: column;
+
+  gap: 1rem;
+}
+</style>
